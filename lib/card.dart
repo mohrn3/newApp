a3cbb552177e5c470e5ea00279d8212e3079aaba
Asith Ishantha <i.madurawalage@diamonhead.tech>
2022-06-08 09:23:57 +0900

    不要なコメントを削除

--- lib/card.dart
+++ lib/card.dart
@@ -1,12 +1,12 @@
 // import 'dart:html';
+// ignore_for_file: deprecated_member_use
+
 import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-// import 'package:newnokosuios/controllers/login_controller.dart';
-// import 'package:newnokosuios/final_Stage.dart';
 import 'package:newnokosuios/image_picker.dart';
 import 'package:line_icons/line_icons.dart';
 import 'package:provider/provider.dart';
@@ -28,8 +28,6 @@ class CardDesign extends StatefulWidget {
   final Cul;
   final posNeg;
 
-  // ImageData imageData;
-
   CardDesign(
       {this.img_string,
       this.person,
@@ -47,20 +45,14 @@ class CardDesign extends StatefulWidget {
 class _CardDesignState extends State<CardDesign> {
   GlobalKey previewContainer = new GlobalKey();
   bool click = true;
-  // String categoryType = widget.category;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           flexibleSpace: Container(
             color: Colors.grey.shade900,
-            // decoration: BoxDecoration(
-            //     gradient: LinearGradient(
-            //         begin: Alignment.topLeft,
-            //         end: Alignment.bottomRight,
-            //         colors: [Colors.green, Colors.blue])),
           ),
-
           actions: [
             FlatButton(
                 onPressed: _captureSocialPng1,
@@ -72,8 +64,6 @@ class _CardDesignState extends State<CardDesign> {
                       fontSize: 12.0),
                 )),
           ],
-
-          // title: Text(widget.title),
         ),
         body: Container(
           padding: EdgeInsets.all(16.0),
@@ -82,12 +72,6 @@ class _CardDesignState extends State<CardDesign> {
               child: Column(
                 children: [
                   buildCard(context),
-                  // buildCard(model, context),
-                  // buildCard(model, context),
-                  // buildCard(model, context),
-                  // buildCard(model, context),
-                  // buildCard(model, context),
-                  // buildCard(model, context),
                 ],
               )),
         ));
@@ -110,8 +94,6 @@ class _CardDesignState extends State<CardDesign> {
     return Card(
       color: Colors.white,
       elevation: 4.0,
-      // margin: EdgeInsets.zero,
-      // clipBehavior: Clip.antiAlias,
 
       // elevation: 4.0,
       child: RepaintBoundary(
@@ -123,19 +105,10 @@ class _CardDesignState extends State<CardDesign> {
               color: Colors.white,
               border: Border.all(
                 width: 0,
-                // color: Colors.grey.shade500,
-
-                // color: Colors.orange.shade400,
               ),
             ),
             child: Column(
               children: [
-                // ListTile(
-                //   title: showCondition(),
-                //   subtitle: PostiveNegative(),
-                //   // trailing: Icon(Icons.favorite_outline),
-                //   // trailing: showCondition(),
-                // ),
                 SizedBox(
                   //Use of SizedBox
                   height: 10,
@@ -145,25 +118,10 @@ class _CardDesignState extends State<CardDesign> {
                       topLeft: Radius.circular(5),
                       topRight: Radius.circular(5)),
                   child: Container(
-                    // margin: EdgeInsets.fromLTRB(5, 5, 5, 0),
                     padding: EdgeInsets.fromLTRB(5, 1, 0, 0),
-                    // color: Colors.grey.shade200,
-                    // width: MediaQuery.of(context).size.width * 0.8,
                     height: 32.0,
                     child: Row(
                       children: [
-                        // Align(
-                        //   child: CircleAvatar(
-                        //     backgroundImage:
-                        //         Image.network(model.userDetails!.photoURL ?? "")
-                        //             .image,
-                        //     radius: 22,
-                        //   ),
-                        // ),
-                        // SizedBox(
-                        //   //Use of SizedBox
-                        //   width: 10,
-                        // ),
                         Align(
                           alignment: Alignment.center,
                           child: Text(username,
@@ -176,54 +134,29 @@ class _CardDesignState extends State<CardDesign> {
                     ),
                   ),
                 ),
-
                 SizedBox(
                   //Use of SizedBox
                   height: 8,
                 ),
                 ClipRRect(
-                  // borderRadius: BorderRadius.only(
-                  //     topLeft: Radius.circular(10),
-                  //     topRight: Radius.circular(10)),
                   child: Container(
-                    // color: Colors.grey.shade200,
                     height: MediaQuery.of(context).size.height * 0.5,
-                    // width: MediaQuery.of(context).size.width * 0.8,
                     decoration: BoxDecoration(
                         color: Colors.grey.shade100,
-                        // border: Border.all(
-                        //   width: 2,
-                        //   // color: Colors.white,
-                        //   color: (widget.posNeg == "true")
-                        //       ? Colors.orange.shade400
-                        //       : Colors.purple.shade400,
-                        //   // color: Colors.orange.shade400,
-                        // ),
                         image: DecorationImage(
-                          //  image: Image.asset('images/cat.png'),
                           image: Image.memory(base64Decode(widget.img_string))
                               .image,
                           fit: BoxFit.fitWidth,
-                          // color: Colors.grey.shade200,
                         )),
                   ),
                 ),
-
                 ClipRRect(
-                  // borderRadius: BorderRadius.only(
-                  //     bottomLeft: Radius.circular(10),
-                  //     bottomRight: Radius.circular(10)),
                   child: Container(
-                    // width: MediaQuery.of(context).size.width * 0.8,
-
                     color: (widget.posNeg == "true")
                         ? Colors.red[300]
                         : Colors.blue.shade400,
                     height: 16.0,
-                    child: Row(
-                        // mainAxisSize: MainAxisSize.max,
-                        // mainAxisAlignment: MainAxisAlignment.start,
-                        ),
+                    child: Row(),
                   ),
                 ),
                 SizedBox(
@@ -231,16 +164,9 @@ class _CardDesignState extends State<CardDesign> {
                   height: 15,
                 ),
                 ClipRRect(
-                  // borderRadius: BorderRadius.only(
-                  //     topLeft: Radius.circular(10),
-                  //     topRight: Radius.circular(10)
-                  //     ),
                   child: Container(
                     padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
-                    // width: MediaQuery.of(context).size.width * 0.8,
-
                     height: 30.0,
-                    // color: Colors.grey.shade200,
                     child: Stack(
                       children: <Widget>[
                         Positioned(
@@ -248,34 +174,25 @@ class _CardDesignState extends State<CardDesign> {
                             child: Container(
                               child: showConditionPhysical(),
                             )),
-
                         Positioned(
                             left: 45,
                             child: Container(
                               child: showConditionCultural(),
                             )),
-
                         Positioned(
                             left: 85,
                             child: Container(
                               child: showConditionEmotional(),
                             )),
-                        // showCondition(),
-
                         Positioned(
                           top: 7,
                           right: 15,
                           child: Container(
                               alignment: Alignment.center,
-                              // alignment: Ali,
-                              // color: Colors.grey.shade200,
-                              // width: MediaQuery.of(context).size.width * 0.8,
-                              // padding: EdgeInsets.all(8.0),
                               padding: EdgeInsets.fromLTRB(45, 0, 0, 0),
                               child: Text.rich(
                                 TextSpan(
                                   children: <InlineSpan>[
-                                    // TextSpan(text: 'Our application already has'),
                                     WidgetSpan(
                                         alignment: ui.PlaceholderAlignment.top,
                                         child: Icon(
@@ -288,48 +205,16 @@ class _CardDesignState extends State<CardDesign> {
                                 ),
                                 textAlign: TextAlign.center,
                                 style: TextStyle(fontSize: 13),
-                              )
-                              // Text(widget.location),
-
-                              ),
+                              )),
                         ),
-
-                        // Positioned(
-                        //   right: 2,
-                        //   child: Text(UserName1,
-                        //       style: const TextStyle(color: Colors.black)),
-                        // ),
-                        // Positioned(
-                        //   right: 2,
-                        //   child: Text(             formattedDate,
-                        //       style: const TextStyle(color: Colors.black)),
-                        // ),
                       ],
                     ),
                   ),
                 ),
-                // SizedBox(
-                //   //Use of SizedBox
-                //   height: 20,
-                // ),
-                // Container(
-                //   color: (widget.posNeg == "true")
-                //       ? Colors.orange.shade400
-                //       : Colors.purple.shade400,
-                //   height: 50.0,
-                //   child: Row(
-                //     mainAxisSize: MainAxisSize.max,
-                //     mainAxisAlignment: MainAxisAlignment.center,
-                //   ),
-                // ),
-
                 ClipRRect(
-                  // borderRadius: BorderRadius.all(Radius.circular(10)),
                   child: Container(
-                    // color: Colors.grey.shade100,
                     padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
                     width: MediaQuery.of(context).size.width * 0.8,
-                    // padding: EdgeInsets.all(8.0),
                     alignment: Alignment.centerLeft,
                     child: Text(supportingText,
                         style: const TextStyle(
@@ -339,7 +224,6 @@ class _CardDesignState extends State<CardDesign> {
                         )),
                   ),
                 ),
-
                 SizedBox(
                   //Use of SizedBox
                   width: 34,
@@ -347,7 +231,6 @@ class _CardDesignState extends State<CardDesign> {
                 Container(
                   padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
                   width: MediaQuery.of(context).size.width * 0.8,
-                  // padding: EdgeInsets.fromLTRB(5, 5, 0, 10),
                   alignment: Alignment.centerLeft,
                   child: Text(formattedDate1,
                       style: const TextStyle(
@@ -362,47 +245,10 @@ class _CardDesignState extends State<CardDesign> {
                     children: [
                       Align(
                         alignment: Alignment.topRight,
-
-                        // child: FlatButton(
-                        //   child: Text(
-                        //     'Download',
-                        //     style: TextStyle(fontSize: 10.0),
-                        //   ),
-                        //   color: Colors.black,
-                        //   textColor: Colors.white,
-                        //   onPressed: _captureSocialPng1,
-                        // ),
-
-                        // child: IconButton(
-                        //   icon: Icon(Icons.download),
-                        //   iconSize: 20,
-                        //   alignment: Alignment.centerRight,
-                        //   onPressed: _captureSocialPng1,
-                        // ),
                       ),
                     ],
                   ),
                 ),
-                // ButtonBar(
-                //     // children: [
-                //     //   TextButton(
-                //     //     child: const Text('CONTACT AGENT'),
-                //     //     onPressed: () {/* ... */},
-                //     //   ),
-                //     //   TextButton(
-                //     //     child: const Text('LEARN MORE'),
-                //     //     onPressed: () {/* ... */},
-                //     //   )
-                //     // ],
-                //     ),
-                // Positioned(
-                //   child: IconButton(
-                //     icon: Icon(Icons.download),
-                //     iconSize: 32,
-                //     alignment: Alignment.center,
-                //     onPressed: _captureSocialPng1,
-                //   ),
-                // ),
               ],
             ),
           ),
@@ -421,15 +267,12 @@ class _CardDesignState extends State<CardDesign> {
 
   showConditionPhysical() {
     if (widget.Phy == "true") {
-      // return Icon(Icons.crop_square_outlined, color: Colors.blue);
-
       return ImageIcon(
         AssetImage("assets/nokosubox.png"),
         color: Colors.blue[700],
         size: 24,
       );
     } else {
-      // return Icon(Icons.crop_square_outlined, color: Colors.grey.shade100);
       return ImageIcon(
         AssetImage("assets/nokosubox.png"),
         color: Colors.grey.shade300,
@@ -440,15 +283,12 @@ class _CardDesignState extends State<CardDesign> {
 
   showConditionCultural() {
     if (widget.Cul == "true") {
-      // return Icon(Icons.circle, color: Colors.green);
       return ImageIcon(
         AssetImage("assets/nokosuearth.png"),
         color: Colors.teal,
         size: 24,
       );
     } else {
-      // return Icon(Icons.circle, color: Colors.grey.shade100);
-
       return ImageIcon(
         AssetImage("assets/nokosuearth.png"),
         color: Colors.grey.shade300,
@@ -459,14 +299,12 @@ class _CardDesignState extends State<CardDesign> {
 
   showConditionEmotional() {
     if (widget.Emo == "true") {
-      // return Icon(Icons.favorite_outline, color: Colors.red);
       return ImageIcon(
         AssetImage("assets/nokosuheart.png"),
         color: Colors.red.shade300,
         size: 24,
       );
     } else {
-      // return Icon(Icons.favorite_outline, color: Colors.grey.shade100);
       return ImageIcon(
         AssetImage("assets/nokosuheart.png"),
         color: Colors.grey.shade300,
@@ -474,19 +312,6 @@ class _CardDesignState extends State<CardDesign> {
       );
     }
   }
-
-  // showCondition() {
-  //   if (widget.category == "Cultural") {
-  //     // PostiveNegative();
-  //     return Icon(Icons.circle, color: Colors.green);
-  //   } else if (widget.category == "Physical") {
-  //     // PostiveNegative();
-  //     return Icon(Icons.circle, color: Colors.blue);
-  //   } else {
-  //     // PostiveNegative();
-  //     return Icon(Icons.favorite_outline, color: Colors.red);
-  //   }
-  // }
 
   Future<void> _captureSocialPng1() {
     List<String> imagePaths = [];
@@ -496,7 +321,7 @@ class _CardDesignState extends State<CardDesign> {
           .findRenderObject() as RenderRepaintBoundary?;
 
       ui.Image image = await boundary!.toImage(pixelRatio: 2.0);
-      // ui.Image image = await boundary!.toImage();
+
       final directory = (await getApplicationDocumentsDirectory()).path;
 
       ByteData? byteData =
